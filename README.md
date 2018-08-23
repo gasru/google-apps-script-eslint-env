ee3fc9008fd2837622fa9687ddd1523a04b4c5a4
Alexander Ivanov <oshli.a.er@gmail.com>
2018-08-23 13:18:11 +0300

    Init

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # google-apps-script-eslint-env
+
+# Установка
+
+```sh
+$> git clone https://github.com/gasru/google-apps-script-eslint-env.git
+$> npm i
+$> npm run run-fix
+```
+
+# Проверка правок
+
+После внесения изменений в файл `src/Code.js` можно запустить перепроверку
+```sh
+$> npm run re-run-fix
+```
---
+++ src/Code.js
@@ -0,0 +1,27 @@
+function getFuckingRange() {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('shema');
+  var range = sheet.getRange('B47:I64');
+  var buff = range.getValues();
+  return buff;
+}
+
+function tryToMakeDocFile() {
+  var buff = getFuckingRange();
+  var doc = DocumentApp.openById('-j-Zp3Q6Lo');
+  var body = doc.getBody();
+  var tableVariantOne = [['hi']];
+  var style = {};
+  style[DocumentApp.Attribute.FONT_FAMILY] = 'Arial';
+  style[DocumentApp.Attribute.FONT_SIZE] = 12;
+  style[DocumentApp.Attribute.BOLD] = true;
+
+  for (var i in buff)
+    if (i != 0) {
+      if ((buff[i][3] = []))
+        var par = body
+          .insertParagraph(i, i + '. ' + buff[i][1])
+          .setHeading(DocumentApp.ParagraphHeading.HEADING1)
+          .setAttributes(style);
+      body.appendTable(tableVariantOne);
+    }
+}
